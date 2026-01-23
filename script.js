8ecf95748b2037a8d74198203e0ed72ddaccf384
Ayushi Patwal <ayushipatwal03@gmail.com>
2026-01-23 13:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let currentAudio = null;
+
+
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", () => {
+    const soundName = button.getAttribute("data-sound");
+
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+  });
+});
+
+
+document.querySelector(".stop").addEventListener("click", () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
